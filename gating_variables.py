fa89c12ec8691d7a4e348740a344ef45e29c00f2
Tiz Kap <tizia@physik.hu-berlin.de>
2018-05-25 11:19:42 +0200

    gating variables adjusted and corrected

--- gating_variables.py
+++ gating_variables.py
@@ -5,46 +5,50 @@ Created on Thu May 24 16:37:06 2018
 
 @author: tiz
 """
-#%% defined params
-eps = 10**(-12)
+#%% import scheme
+import numpy as np
+
+#%% defined params and import schemes
+eps = 10**(-12)  # for determining division by zero limes
 
 #%% potassium (K) gating variable n
+
 
 def alpha_n(V):
     ''' K gating variable n dynamics,
     including exception for div. by zero,
     solved with l'hospital'''
-    if abs(V + 55) < eps:
+    if abs(V + 60) < eps:
         return .1
-    return (.01 * (V+55)) / (1 - np.exp(-.1 * (V+55))) 
-    
+    return (.01 * (V+60)) / (1 - np.exp(-.1 * (V+60)))
+
 
 def beta_n(V):
-    ''' K gating variable n dynamics,'''
-    return .125 * np.exp(-.0125 * (V+65))
-    
+    ''' K gating variable n dynamics'''
+    return .125 * np.exp(-.0125 * (V+70))
+
 #%% sodium (Na) gating variables m and h
 
 
 def alpha_m(V):
-    ''' sodium m channel voltage dependent transition rate'''
-    if abs(V + 40) < eps:
+    '''Na gating variable m dynamics,
+    including exception for div. by zero,
+    solved with l'hospital'''
+    if abs(V + 45) < eps:
         return 1.0
-    return (.1 * (V+40)) / (1 - np.exp(-.1 * (V+40)))
+    return (.1 * (V+45)) / (1 - np.exp(-.1 * (V+45)))
 
 
 def alpha_h(V):
-    ''' sodium h channel voltage dependent transition rate'''
-    return .07 * np.exp(-.05 * (V+65))
+    '''Na gating variable h dynamics'''
+    return .07 * np.exp(-.05 * (V+70))
 
 
 def beta_m(V):
-    ''' sodium m channel voltage dependent transition rate'''
-    return 4 * np.exp(-.0556 * (V+65))
+    '''Na gating variable m dynamics'''
+    return 4 * np.exp(-(V+70)/18)
 
 
 def beta_h(V):
-    ''' sodium h channel voltage dependent transition rate'''
-    if abs(V + 35) < eps:
-        return .5
-    return 1 / (1 + np.exp(-.1 * (V+35)))
+    '''Na gating variable h dynamics'''
+    return 1 / (1 + np.exp(-.1 * (V+40)))
