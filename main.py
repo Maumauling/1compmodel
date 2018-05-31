17045f3d54e3b7718a6f391c50c53f27623b8df6
Tiz Kap <tizia@physik.hu-berlin.de>
2018-05-31 11:25:04 +0200

    added I_syn for plotting, running model version, todo: units

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ Created on Fri May 25 13:20:06 2018
 #%% global import schemes
 
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 from scipy import integrate
+from matplotlib.ticker import FormatStrFormatter
 
 
 #%% needed functions and parameters import
@@ -46,3 +48,34 @@ def mhnV(y, t, C, g_Na, E_Na, g_K, E_K, g_leak, E_leak, g_DC, g_AC, f, delta, E_
     return dmdt, dhdt, dndt, dVdt
 
 sol = integrate.odeint(mhnV, [m0, h0, n0, V0], t_range, args = (C_som, g_Na_som, E_Na, g_K_som, E_K, g_leak_som, E_leak, g_DC, g_AC, f, delta, E_syn))
+
+ina = I_Na(sol[:,0], sol[:,1], sol[:,3], g_Na_som, E_Na)
+ik = I_K(sol[:,2], sol[:,3], g_K_som, E_K)
+ileak = I_leak(sol[:,3], g_leak_som, E_leak)
+isyn = I_syn(sol[:,3], t_range, g_DC, g_AC, f, delta, E_syn)
+
+'''PLOTSPECS'''
+
+fig = plt.figure(dpi=85)
+
+gs = gridspec.GridSpec(8, 2)
+gs.update(hspace=0.7)
+ax1 = plt.subplot(gs[0:2, :])
+ax2 = plt.subplot(gs[2:4, :])
+ax3 = plt.subplot(gs[4:6, :])
+ax4 = plt.subplot(gs[6:8, :])
+
+ax1.plot(t_range, sol[:,3])
+ax2.plot(t_range, sol[:,0], t_range, sol[:,1], t_range, sol[:,2])
+ax3.plot(t_range, ina, t_range, ik, t_range, ileak)
+ax4.plot(t_range, isyn)
+
+plt.setp(ax1.get_xticklabels(), visible=False)
+plt.setp(ax2.get_xticklabels(), visible=False)
+ax1.set_ylabel('voltage []', size=10)
+ax1.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+ax2.set_ylabel('m,h,n \n channel', size=10)
+ax2.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+ax3.set_ylabel('INa,IK,Ileak', size=10)
+ax3.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+ax3.set_xlabel('time [ms]', size=10)
