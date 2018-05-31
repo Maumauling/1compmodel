752649ce5af09db27314fc69e9b39159e7edd588
Tiz Kap <tizia@physik.hu-berlin.de>
2018-05-31 15:05:29 +0200

    made proposal for plotting routine structure TODO

--- plotting_mhnV.py
+++ plotting_mhnV.py
@@ -6,5 +6,14 @@ Created on Thu May 31 14:04:26 2018
 @author: Tizia Kaplan
 
 plotting routine for data generated via 1compmodel; mhnV runge kutta solution
+
+wanted structure: if channelplotting = True -> in plot also channel kinetics, but can be only membrane voltage and so on. 
+
+proposed structure:
+    first define boolean variables for how much in plot included
+    second go into if loop
+    
+    maybe beforehand plotting fcts need to be defined
+    
 """
 
